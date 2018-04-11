c83119778466268c0632bff9797306661c81d34a
miyatheta <miyatheta69@gmail.com>
2018-04-11 20:36:12 +0900

    validate

--- db/migrate/20180410111834_create_users.rb
+++ db/migrate/20180410111834_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :pasword
-      t.string :confirm_pasword
+      t.string :password
+      t.string :password_confirmation
       t.date :birthday
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20180410111834) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "pasword"
-    t.string "confirm_pasword"
+    t.string "password"
+    t.string "password_confirmation"
     t.date "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
