64a799308c921245a7d9de20b2e21a4ecfa0f639
miyatheta <miyatheta69@gmail.com>
2018-04-12 20:56:55 +0900

    gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'sign_up' => 'users#new'
 
   get '/' => 'home#top'
-
+  get 'sign_up' => 'users#new'
   get 'about' => 'home#about'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
