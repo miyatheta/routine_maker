54efad005d4a8c0653e10d1ab7322bda896b91b0
miyatheta <miyatheta69@gmail.com>
2018-04-13 21:34:24 +0900

    ruby turorial 7.3.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   get '/' => 'home#top'
-  get 'sign_up' => 'users#new'
-  get 'about' => 'home#about'
+  get '/about' => 'home#about'
+  get '/sign_up' => 'users#new'
+  post '/sign_up' => 'users#create'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
