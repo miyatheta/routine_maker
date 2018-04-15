4f624d8deebfdfcd7417a71302311ef9eb711f2e
miyatheta <miyatheta69@gmail.com>
2018-04-15 16:59:54 +0900

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/about' => 'home#about'
   get '/sign_up' => 'users#new'
   post '/sign_up' => 'users#create'
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/logout' => 'session#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
