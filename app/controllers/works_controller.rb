1dddb2d80c9b6212403582673dbb1f74a09d0624
miyatheta <miyatheta69@gmail.com>
2018-04-20 21:47:26 +0900

    indivisual detail

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,8 @@ class WorksController < ApplicationController
   def index
     @works=Work.all
   end
+
+  def show
+    @work=Work.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root 'home#top'
   get '/about' => 'home#about'
+  get '/index'=> 'works#index'
+  get 'works/:id'=> 'works#show'
   get '/sign_up' => 'users#new'
   post '/sign_up' => 'users#create'
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/logout' => 'session#destroy'
-  get '/index'=> 'works#index'
+  get 'users/:id' => 'users#show'
+
 
   resources :users
 
